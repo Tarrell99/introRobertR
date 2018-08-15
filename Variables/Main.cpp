e09c33431d559f5c3fcdc3d1e32236ab1274cc5d
tarrellriley <tarrellriley@gmail.com>
2018-08-15 17:58:31 -0500

    add pro 1-8

--- Variables/Main.cpp
+++ Variables/Main.cpp
@@ -36,6 +36,81 @@ int main()
 	std::cout << userInput;
 
 
+	//Problem 1. my guess is =14
+	
+	// main.cpp/main()
+	// ...
+	// A)
+	int numberA = 5;
+	numberA = 9;
+	numberA = 11;
+	numberA = 14;
+	std::cout << "A) " << numberA << std::endl;
+
+	//Problem 2. my guess is =9
+
+	// main.cpp/main()
+	// ...
+	// B)
+	int numberB = 10;
+	numberB = 9;
+	int thingB = 55;
+	thingB = 22;
+	std::cout << "B) " << numberB << std::endl;
+
+	//Problem 3. my guess is =21
+
+	// main.cpp/main()
+	// ...
+	// C)
+	int numberC = 3;
+	numberC = 7;
+	numberC = 1;
+	int somethingC = 23;
+	somethingC = 21;
+	numberC = somethingC;
+	std::cout << "C) " << numberC << std::endl;
+
+	//Problem 4. my guess is =3
+
+	// main.cpp/main()
+	// ...
+	// D)
+	int numberD = 1;
+	int somethingD = 3;
+	numberD = somethingD;
+	std::cout << "D) " << somethingD << std::endl;
+	//Problem 5. my guess is =12  I know what i miss it goes in 6 times with a remeder 1 .
+
+	// main.cpp/main()
+	// ...
+	// E)
+	int x = 13;
+	x = x / 2;
+	std::cout << "E) " << x << std::endl;
+
+	//problem 6. my guess is =6.1 I know what did wroge it is on a count of 1 and not 10 EX. 6,6.5,7 V.S 6,6.1,6.2,6.3....
+
+	// main.cpp/main()
+	// ...
+	// F)
+	float y = 13;
+	y = y / 2;
+	std::cout << "F)" << y << std::endl;	// main.cpp/main()
+	// ...
+	// Celsius to Fahrenheit)
+	float degCelsius = 78.0f;
+	float degFahrenheit = 0.0f; 
+
+	// <Your work goes here>
+	float myver = 0;
+	 myver = 78.0 * 1.8 + 32;
+ 
+	std::cout << "Celsius to Fahrenheit)" << std::endl;
+	std::cout << "Celsius: " << degCelsius << std::endl;
+	std::cout << "Fahrenheit: " << myver << std::endl;
+
+
 
 	system("pause");
 
