953820172cc67cb5aafdc56b9a70d76cf43edc51
tarrellriley <tarrellriley@gmail.com>
2018-08-17 12:36:06 -0500

    worked on all probles but two.

--- Variables/Main.cpp
+++ Variables/Main.cpp
@@ -2,7 +2,8 @@
 
 int main()
 {
-	int number;
+	//walkthrough
+	{int number;
 
 	number = 18;
 	std::cout << number << std::endl;
@@ -34,103 +35,110 @@ int main()
 	std::cout << "give me a whole number" << std::endl;
 	std::cin >> userInput;
 	std::cout << userInput;
+	}
+	// Problem A
+	{//Problem 1. my guess is =14
 
-
-	//Problem 1. my guess is =14
-	
 	// main.cpp/main()
 	// ...
 	// A)
-	int numberA = 5;
-	numberA = 9;
-	numberA = 11;
-	numberA = 14;
-	std::cout << "A) " << numberA << std::endl;
-
-	//Problem 2. my guess is =9
+		int numberA = 5;
+		numberA = 9;
+		numberA = 11;
+		numberA = 14;
+		std::cout << "A) " << numberA << std::endl;
+	}
+	// Problem B
+	{//Problem 2. my guess is =9
 
 	// main.cpp/main()
 	// ...
 	// B)
-	int numberB = 10;
-	numberB = 9;
-	int thingB = 55;
-	thingB = 22;
-	std::cout << "B) " << numberB << std::endl;
-
-	//Problem 3. my guess is =21
+		int numberB = 10;
+		numberB = 9;
+		int thingB = 55;
+		thingB = 22;
+		std::cout << "B) " << numberB << std::endl;
+	}
+	// Problem C
+	{//Problem 3. my guess is =21
 
 	// main.cpp/main()
 	// ...
 	// C)
-	int numberC = 3;
-	numberC = 7;
-	numberC = 1;
-	int somethingC = 23;
-	somethingC = 21;
-	numberC = somethingC;
-	std::cout << "C) " << numberC << std::endl;
-
-	//Problem 4. my guess is =3
+		int numberC = 3;
+		numberC = 7;
+		numberC = 1;
+		int somethingC = 23;
+		somethingC = 21;
+		numberC = somethingC;
+		std::cout << "C) " << numberC << std::endl;
+	}
+	// Problem D
+	{//Problem 4. my guess is =3
 
 	// main.cpp/main()
 	// ...
 	// D)
-	int numberD = 1;
-	int somethingD = 3;
-	numberD = somethingD;
-	std::cout << "D) " << somethingD << std::endl;
-	//Problem 5. my guess is =12  I know what i miss it goes in 6 times with a remeder 1 .
+		int numberD = 1;
+		int somethingD = 3;
+		numberD = somethingD;
+		std::cout << "D) " << somethingD << std::endl;	}
+	// Problem E
+	{//Problem 5. my guess is =12  I know what i miss it goes in 6 times with a remeder 1 .
 
 	// main.cpp/main()
 	// ...
 	// E)
-	int x = 13;
-	x = x / 2;
-	std::cout << "E) " << x << std::endl;
-
-	//problem 6. my guess is =6.1 I know what did wroge it is on a count of 1 and not 10 EX. 6,6.5,7 V.S 6,6.1,6.2,6.3....
+		int x = 13;
+		x = x / 2;
+		std::cout << "E) " << x << std::endl;
+	}
+	// Problem F
+	{//problem 6. my guess is =6.1 I know what did wroge it is on a count of 1 and not 10 EX. 6,6.5,7 V.S 6,6.1,6.2,6.3....
 
 	// main.cpp/main()
 	// ...
 	// F)
-	float y = 13;
-	y = y / 2;
-	std::cout << "F)" << y << std::endl;	// main.cpp/main()
-	// ...
+		float y = 13;
+		y = y / 2;
+		std::cout << "F)" << y << std::endl;	}	// Celsius to Fahrenheit 
+	{// changeing the variable with a float 
 	// Celsius to Fahrenheit)
-	float degCelsius = 78.0f;
-	float degFahrenheit = 0.0f; 
+		float degCelsius = 78.0f;
+		float degFahrenheit = 0.0f;
 
-	// <Your work goes here>
-	float myver = 0;
-	 myver = 78.0 * 1.8 + 32;
- 
-	std::cout << "Celsius to Fahrenheit)" << std::endl;
-	std::cout << "Celsius: " << degCelsius << std::endl;
-	std::cout << "Fahrenheit: " << myver << std::endl;
-	float rectWidth = 0.0f;
+		// <Your work goes here>
+		float myver = 0;
+		myver = 78.0 * 1.8 + 32;
+
+		std::cout << "Celsius to Fahrenheit)" << std::endl;
+		std::cout << "Celsius: " << degCelsius << std::endl;
+		std::cout << "Fahrenheit: " << myver << std::endl;	}
+	// Area of a Rectangle
+	{
+		float rectWidth = 0.0f;
 	float rectHeight = 0.0f;
 	float rectArea = 0.0f;
 
 	std::cout << "Area of a Rectangle)" << std::endl;
-	
+
 	std::cout << "what is the height" << std::endl;
 	std::cin >> rectHeight;
-	
+
 	std::cout << "what is the width" << std::endl;
 	std::cin >> rectWidth;
 
 	std::cout << "H: " << rectHeight << " , W: " << rectWidth << std::endl;
 	std::cout << "Area: " << rectHeight * rectWidth << std::endl;
-
-
-	float a, b, c, d, e;
+	}
+	// Average of Five
+	{float a, b, c, d, e;
 	float avg;
 	avg = a = b = c = d = e = 0.0f;
 	std::cout << "Average of Five)" << std::endl;
 
-	
+
 	std::cout << " What is a" << std::endl;
 	std::cin >> a;
 
@@ -147,31 +155,79 @@ int main()
 	std::cin >> e;
 
 	std::cout << a << "," << b << "," << c << "," << d << "," << e << std::endl;
-	std::cout << "avg: " << ( a + b + c + d + e ) / 5 << std::endl;
+	std::cout << "avg: " << (a + b + c + d + e) / 5 << std::endl;
+	}
+	// Number Swap 
+	{
+		std::cout << "Number Swap)" << std::endl;
 
-	std::cout << "Number Swap)" << std::endl;
-	
-	int r;
-	int m;
-	
-	std::cout << "r is..." << std::endl;
-	std::cin >> r;
-	
-	std::cout << "m is..." << std::endl;
-	std::cin >> m;
+		int r;
+		int m;
 
-	std::cout << "r is " << m << std::endl;
-	std::cout << "m is " << r << std::endl;
+		std::cout << "r is..." << std::endl;
+		std::cin >> r;
 
-	int age;
+		std::cout << "m is..." << std::endl;
+		std::cin >> m;
+
+		std::cout << "r is " << m << std::endl;
+		std::cout << "m is " << r << std::endl;
+	}
+
+	// Fun Facts for Your Age 
+	{int age;
 	std::cout << " what intarnaton how old are you" << std::endl;
 	std::cin >> age;
 
-	
+
 	std::cout << "Howdy! You are " << age << " years old!" << std::endl;
 	std::cout << "you about " << (12 * age) << " months" << std::endl;
+	}
+
+	//// Confarming to Convention
+	//	{// Challenge #1
+	//
+	//	 //int age;
+	//	 //int age;
+	//
+	//	 //int bulletCount;
+	//	 //int bullet_count;
+	//
+	//	 //float dogYears;
+	//	 //float dog_years;
+	//
+	//	 //int qty;
+	//	 //int qty;
+	//
+	//	 //float shieldValue;
+	//	 //float shied_value;
+	//
+	//	 //float defenseMatrixCooldown;
+	//	 //float defense_matrix_cooldown;
+	//
+	//	 //int redArmorValue;
+	//	 //int red_armor_value;
+	//
+	//	 //float redArmorRatio;
+	//	 //float red_armor_ratio;
+	//
+	//	 //int happiness;
+	//	 //int happiness;
+	//	//	 //int gandhiAggression;	//	 //int gandhi_aggression;	//	}		//	// the right tool for the job	//	{// Challenge #2		int num;  // integer	float num2;  // single precision floating point	double num3;	char num4;	void array;	array num6;	short num7;	long num8;	bool num9;		
+
+
+
+
+
+
+
+
+
+
 
 	system("pause");
 
 }
+
+
 
